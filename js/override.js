2f5763079c6533d37837ee02d207223af99bbcb2
Brady Dowling <bradydowling@gmail.com>
2015-04-13 21:30:22 +0000

    Commiting version 0.0.1

---
+++ js/override.js
@@ -0,0 +1,421 @@
+var scriptures = [
+	{
+		"content": "And I said unto him: I know that he loveth his children; nevertheless, I do not know the meaning of all things.",
+		"author": "1 Nephi 11:17"
+	},
+	{
+		"content": "But behold, I, Nephi, will show unto you that the tender mercies of the Lord are over all those whom he hath chosen, because of their faith, to make them mighty even unto the power of deliverance.",
+		"author": "1 Nephi 1:20"
+	},
+	{
+		"content": "For, behold, I have refined thee, I have chosen thee in the furnace of affliction.",
+		"author": "1 Nephi 20:10"
+	},
+	{
+		"content": "Sing, O heavens; and be joyful, O earth; for the feet of those who are in the east shall be established; and break forth into singing, O mountains; for they shall be smitten no more; for the Lord hath comforted his people, and will have mercy upon his afflicted.",
+		"author": "1 Nephi 21:13"
+	},
+	{
+		"content": "And it came to pass that I, Nephi, said unto my father: I will go and do the things which the Lord hath commanded, for I know that the Lord giveth no commandments unto the children of men, save he shall prepare a way for them that they may accomplish the thing which he commandeth them.",
+		"author": "1 Nephi 3:7"
+	},
+	{
+		"content": "But the Lord knoweth all things from the beginning; wherefore, he prepareth a way to accomplish all his works among the children of men; for behold, he hath all power unto the fulfilling of all his words. And thus it is. Amen.",
+		"author": "1 Nephi 9:6"
+	},
+	{
+		"content": "Behold, hath the Lord commanded any that they should not partake of his goodness? Behold I say unto you, Nay; but all men are privileged the one like unto the other, and none are forbidden.",
+		"author": "2 Nephi 26:28"
+	},
+	{
+		"content": "And now, Jacob, I speak unto you: Thou art my first-born in the days of my tribulation in the wilderness. And behold, in thy childhood thou hast suffered afflictions and much sorrow, because of the rudeness of thy brethren. <br><br>Nevertheless, Jacob, my first-born in the wilderness, thou knowest the greatness of God; and he shall consecrate thine afflictions for thy gain.",
+		"author": "2 Nephi 2:1-2"
+	},
+	{
+		"content": "Adam fell that men might be; and men are, that they might have joy.",
+		"author": "2 Nephi 2:25"
+	},
+	{
+		"content": "For my soul delighteth in plainness; for after this manner doth the Lord God work among the children of men. For the Lord God giveth light unto the understanding; for he speaketh unto men according to their language, unto their understanding.",
+		"author": "2 Nephi 31:3"
+	},
+	{
+		"content": "And all thy children shall be taught of the Lord; and great shall be the peace of thy children.",
+		"author": "3 Nephi 22:13"
+	},
+	{
+		"content": "The spirit and the body shall be reunited again in its perfect form.",
+		"author": "Alma 11:43"
+	},
+	{
+		"content": "Whosoever shall put their trust in God shall be supported in their trials, and their troubles, and their afflictions, and shall be lifted up at the last day.",
+		"author": "Alma 36:3"
+	},
+	{
+		"content": "The soul shall be restored to the body, and the body to the soul; yea, and every limb and joint shall be restored to its body; yea, even a hair of the head shall not be lost; but all things shall be restored to their proper and perfect frame.",
+		"author": "Alma 40:23"
+	},
+	{
+		"content": "Now behold, it was not expedient that man should be reclaimed from this temporal death, for that would destroy the great plan of happiness.",
+		"author": "Alma 42:8"
+	},
+	{
+		"content": "And he shall go forth, suffering pains and afflictions and temptations of every kind; and this that the word might be fulfilled which saith he will take upon him the pains and the sicknesses of his people. <br><br>And he will take upon him death, that he may loose the bands of death which bind his people; and he will take upon him their infirmities, that his bowels may be filled with mercy, according to the flesh, that he may know according to the flesh how to succor his people according to their infirmities.",
+		"author": "Alma 7:11-12"
+	},
+	{
+		"content": "And if men come unto me I will show unto them their weakness. I give unto men weakness that they may be humble; and my grace is sufficient for all men that humble themselves before me; for if they humble themselves before me, and have faith in me, then will I make weak things become strong unto them.",
+		"author": "Ether 12:27"
+	},
+	{
+		"content": "Wherefore, whoso believeth in God might with surety hope for a better world, yea, even a place at the right hand of God, which hope cometh of faith, maketh an anchor to the souls of men, which would make them sure and steadfast.",
+		"author": "Ether 12:4"
+	},
+	{
+		"content": "Yea, come unto Christ, and be perfected in him, and deny yourselves of all ungodliness; and if ye shall deny yourselves of all ungodliness, and love God with all your might, mind and strength, then is his grace sufficient for you, that by his grace ye may be perfect in Christ; and if by the grace of God ye are perfect in Christ, ye can in nowise deny the power of God.",
+		"author": "Moroni 10:32"
+	},
+	{
+		"content": "Bear one another's burdens, that they may be light; <br><br>...mourn with those that mourn; yea, and comfort those that stand in need of comfort, and to stand as witnesses of God at all times and in all things, and in all places that ye may be in, even until death, that ye may be redeemed of God, and be numbered with those of the first resurrection, that ye may have eternal life.",
+		"author": "Mosiah 18:8-9"
+	},
+	{
+		"content": "Therefore they did watch over their people, and did nourish them with things pertaining to righteousness.",
+		"author": "Mosiah 23:18"
+	},
+	{
+		"content": "And see that all these things are done in wisdom and order; for it is not requisite that a man should run faster than he has strength. And again, it is expedient that he should be diligent, that thereby he might win the prize; therefore, all things must be done in order.",
+		"author": "Mosiah 4:27"
+	}
+];
+
+var lyrics = [];
+var quotes = [
+	{
+		"content": "When we offer succor to anyone, the Savior feels it as if we reached out to succor Him.",
+		"author": "President Henry B. Eyring"
+	},
+	{
+		"content": "Mature love has a bliss not even imagined by newlyweds.",
+		"author": "President Boyd K. Packer"
+	},
+	{
+		"content": "I am convinced that a husband is never more attractive to his wife than when he is serving in his God-given roles as a worthy priesthood holder.",
+		"author": "Sister Linda K. Burton"
+	},
+	{
+		"content": "When was the last time I chose to be happy rather than demanding to be right?",
+		"author": "Sister Linda K. Burton"
+	},
+	{
+		"content": "In an age dominated by the Internet, which magnifies messages that menace faith, we must increase our exposure to spiritual truth in order to strengthen our faith and stay rooted in the gospel.",
+		"author": "Elder Dallin H. Oaks"
+	},
+	{
+		"content": "We will not accidentally come to believe in the Savior.",
+		"author": "Elder L. Whitney Clayton"
+	},
+	{
+		"content": "Family is the center of life. It is the key to eternal happiness.",
+		"author": "Elder L. Tom Perry"
+	},
+	{
+		"content": "Our marriage ceremonies eliminate the words 'til death do us part' and instead say 'for time and for all eternity.' ",
+		"author": "Elder L. Tom Perry"
+	},
+	{
+		"content": "As we fear God more completely, we love Him more perfectly. And 'perfect love casteth out all fear.' ",
+		"author": "Elder David A. Bednar"
+	},
+	{
+		"content": "We should look to and have our focus firmly fixed upon the Savior at all times and in all places.",
+		"author": "Elder David A. Bednar"
+	},
+	{
+		"content": "I promise the bright light of Godly fear will chase away the dark shadows of mortal fears as we look to the Savior.",
+		"author": "Elder David A. Bednar"
+	},
+	{
+		"content": "May the Lord's grace bring happiness now and faith in sure promises to come.",
+		"author": "Elder D. Todd Christofferson"
+	},
+	{
+		"content": "Much that is good, much that is essential ... can be achieved in less than ideal circumstances.",
+		"author": "Elder D. Todd Christofferson"
+	},
+	{
+		"content": "Everyone has gifts; everyone can contribute.",
+		"author": "Elder D. Todd Christofferson"
+	},
+	{
+		"content": "Our children's happiness depends on their ability to hear and love the beautiful music of the gospel.",
+		"author": "Elder Wilford W. Andersen"
+	},
+	{
+		"content": "God cares a lot more about who we are and about who we are becoming, than about who we once were.",
+		"author": "Elder Dale G. Renlund"
+	},
+	{
+		"content": "Great women and men are always more anxious to serve than to have dominion.",
+		"author": "President Spencer W. Kimball"
+	},
+	{
+		"content": "When we follow the Savior of the world, the Son of God, we flourish and become glorious despite the many terrible circumstances that surround us.",
+		"author": "Elder Quentin L. Cook"
+	},
+	{
+		"content": "As we enter through the doors of the temple, we leave behind us the distractions and confusion of the world.",
+		"author": "President Thomas S. Monson"
+	},
+	{
+		"content": "Christ's peace can permeate any heart - hearts that are troubled, burdened with grief, confused and pleading for help.",
+		"author": "President Thomas S. Monson"
+	},
+	{
+		"content": "Our faith can reach beyond the limits of current reason.",
+		"author": "Sister Rosemary M. Wixom"
+	},
+	{
+		"content": "Tribulations often enrich, refine and guide us to a deeper understanding of the purpose of our existence.",
+		"author": "Elder Jose Teixeira"
+	},
+	{
+		"content": "The more we understand the extraordinary role of Christ in our lives, the more conscious we become of our purpose.",
+		"author": "Elder Jose Teixeira"
+	},
+	{
+		"content": "There is always something new to learn and feel each Sunday.",
+		"author": "Bishop Gerald Causse"
+	},
+	{
+		"content": "All of us need the Savior's Atonement to heal us. All of us are lost and need to be found.",
+		"author": "Elder Brent H. Nielson"
+	},
+	{
+		"content": "Jesus Christ ... suffered, died and rose from death in order that He could, like lightning in a summer storm, grasp us as we fell.",
+		"author": "Elder Jeffrey R. Holland"
+	},
+	{
+		"content": "With the gift of God's grace, the path of discipleship does not lead backward; it leads upward.",
+		"author": "President Dieter F. Uchtdorf"
+	},
+	{
+		"content": "Thinking that we can trade our good works for salvation is like buying a plane ticket and then supposing we own the airline.",
+		"author": "President Dieter F. Uchtdorf"
+	},
+	{
+		"content": "Whoever you are and whatever your past, remember this: It is not too late to make that same choice and follow Him.",
+		"author": "Elder Robert D. Hales"
+	},
+	{
+		"content": "Live your life to be a good example of what you believe.",
+		"author": "Elder Robert D. Hales"
+	},
+	{
+		"content": "If you lose the Spirit, you are lost.",
+		"author": "Elder Kevin W. Pearson"
+	},
+	{
+		"content": "The Lord knows where each piece belongs so that it fits into the plan.",
+		"author": "Elder Rafael E. Pino"
+	},
+	{
+		"content": "Can you see the hand of God in your own life?",
+		"author": "Elder Neil L. Andersen"
+	},
+	{
+		"content": "We believe in Him. We worship Him. We follow Him. We deeply love Him.",
+		"author": "Elder Neil L. Andersen"
+	},
+	{
+		"content": "The path we have chosen to walk is narrow.",
+		"author": "Elder Jorge F. Zeballos"
+	},
+	{
+		"content": "God charged us to be fruitful.",
+		"author": "Elder Joseph W. Sitati"
+	},
+	{
+		"content": "Because of what I have heard and felt during this conference, how will I change?",
+		"author": "Elder Russell M. Nelson"
+	},
+	{
+		"content": "What sign will you give to the Lord to show your love for Him?",
+		"author": "Elder Russell M. Nelson"
+	},
+	{
+		"content": "RM doesn't mean 'Retired Mormon.'",
+		"author": "Elder M. Russell Ballard"
+	},
+	{
+		"content": "Great courage ... will be required to choose the right.",
+		"author": "Elder Ulisses Soares"
+	},
+	{
+		"content": "Your sons will learn how to be husbands and fathers by observing the way you fulfill these roles.",
+		"author": "Brother Larry M. Gibson"
+	},
+	{
+		"content": "Even the smallest act of discipleship or the tiniest ember of belief can become a blazing bonfire of a consecrated life.",
+		"author": "President Dieter F. Uchtdorf"
+	},
+	{
+		"content": "If you feel small and weak, please simply come unto Christ, who makes weak things strong.",
+		"author": "President Dieter F. Uchtdorf"
+	},
+	{
+		"content": "He is the God above all, the father above all, and yet willing to give undivided attention to one of his children.",
+		"author": "President Henry B. Eyring"
+	},
+	{
+		"content": "He is our captain, the King of Glory, even the Son of God.",
+		"author": "President Thomas S. Monson"
+	},
+	{
+		"content": "Strong eternal families and Spirit-filled homes do not just happen. They take great effort, they take time, and they take each member of the family doing his or her part.",
+		"author": "Sister Cheryl A. Esplin"
+	},
+	{
+		"content": "As we fill our hearts and homes with the Savior's light and truth, we will have the inner strength to withstand in every circumstance.",
+		"author": "Sister Cheryl A. Esplin"
+	},
+	{
+		"content": "Families are the Lord's workshop on earth to help us learn and live the gospel.",
+		"author": "Sister Cheryl A. Esplin"
+	},
+	{
+		"content": "'Heavenly Father and His Son, Jesus Christ, are perfect. ... Their hopes for us are perfect.' Their plan for us is perfect, and their promises are sure.",
+		"author": "Sister Carole M. Stephens"
+	},
+	{
+		"content": "We are each part of God's family.",
+		"author": "Sister Carole M. Stephens"
+	},
+	{
+		"content": "Membership in the family of God is not contingent upon any kind of status - marital status, parental status, financial status, social status, or even the kind of status we post on social media.",
+		"author": "Sister Carole M. Stephens"
+	},
+	{
+		"content": "He is the way - the only way - we can overcome mortal challenges, be healed, and return back to our heavenly home.",
+		"author": "Sister Carole M. Stephens"
+	},
+	{
+		"content": "If it is the Lord's plan, it should also be our plan!",
+		"author": "Sister Bonnie L. Oscarson"
+	},
+	{
+		"content": "One of the hardest challenges in this life is to have faith in the Lord's timing.",
+		"author": "Sister Bonnie L. Oscarson"
+	},
+	{
+		"content": "Let us defend the home as a place which is second only to the temple in holiness.",
+		"author": "Sister Bonnie L. Oscarson"
+	},
+	{
+		"content": "His love never fails.",
+		"author": "President Henry B. Eyring"
+	},
+	{
+		"content": "He allowed His Son to provide, by His atoning sacrifice, the hope that comforts us no matter how hard the way home to Him may be.",
+		"author": "President Henry B. Eyring"
+	},
+	{
+		"content": "Listen with love.",
+		"author": "President Henry B. Eyring"
+	},
+	{
+		"content": "Only God knows hearts.",
+		"author": "President Henry B. Eyring"
+	},
+	{
+		"content": "The burdens his faithful servants must carry in this life are made lighter by His Atonement.",
+		"author": "President Henry B. Eyring"
+	}
+];
+
+var images = [
+	"../../img/img1.jpg",
+	"../../img/img2.jpg",
+	"../../img/img3.jpg",
+	"../../img/img4.jpg",
+	"../../img/img5.jpg",
+	"../../img/img6.jpg",
+	"../../img/img7.jpg",
+	"../../img/img8.jpg",
+	"../../img/img9.jpg",
+	"../../img/img10.jpg",
+	"../../img/img11.jpg",
+	"../../img/img12.jpg",
+	"../../img/img13.jpg",
+	"../../img/img14.jpg",
+	"../../img/img15.jpg",
+	"../../img/img16.jpg",
+	"../../img/img17.jpg",
+	"../../img/img18.jpg",
+	"../../img/img19.jpg",
+	"../../img/img20.jpg",
+	"../../img/img21.jpg",
+	"../../img/img22.jpg",
+	"../../img/img23.jpg",
+	"../../img/img24.jpg",
+	"../../img/img25.jpg",
+	"../../img/img26.jpg",
+	"../../img/img27.jpg",
+	"../../img/img28.jpg",
+	"../../img/img29.jpg",
+	"../../img/img30.jpg",
+	"../../img/img31.jpg",
+	"../../img/img32.jpg",
+	"../../img/img33.jpg",
+	"../../img/img34.jpg",
+	"../../img/img35.jpg",
+	"../../img/img36.jpg",
+	"../../img/img37.jpg",
+	"../../img/img38.jpg",
+	"../../img/img39.jpg",
+	"../../img/img40.jpg",
+	"../../img/img41.jpg",
+	"../../img/img42.jpg",
+	"../../img/img43.jpg",
+	"../../img/img44.jpg",
+	"../../img/img45.jpg",
+	"../../img/img46.jpg",
+	"../../img/img47.jpg",
+	"../../img/img48.jpg",
+	"../../img/img49.jpg",
+	"../../img/img50.jpg",
+	"../../img/img51.jpg",
+	"../../img/img52.jpg",
+	"../../img/img53.jpg",
+	"../../img/img54.jpg",
+	"../../img/img55.jpg",
+	"../../img/img56.jpg",
+	"../../img/img57.jpg",
+	"../../img/img58.jpg",
+	"../../img/img59.jpg",
+	"../../img/img60.jpg",
+	"../../img/img61.jpg",
+	"../../img/img62.jpg",
+	"../../img/img63.jpg",
+	"../../img/img64.jpg",
+	"../../img/img65.jpg",
+	"../../img/img66.jpg",
+	"../../img/img67.jpg",
+	"../../img/img68.jpg",
+	"../../img/img69.jpg",
+	"../../img/img70.jpg",
+	"../../img/img71.jpg",
+	"../../img/img72.jpg",
+];
+var imageNumber = Math.round(Math.random() * (images.length - 1));
+console.log(imageNumber);
+var randomImage = images[imageNumber];
+console.log(randomImage);
+
+var items = quotes.concat(lyrics.concat(scriptures));
+var randomNumber = Math.round(Math.random() * (items.length - 1));
+var randomItem = items[randomNumber];
+document.getElementById("content").innerHTML = randomItem.content;
+document.getElementById("author").innerHTML = randomItem.author.length > 0 ? "- " + randomItem.author : "";
+document.getElementById("inspiration").style.visibility = "visible";
+document.body.style.backgroundImage = "url('" + randomImage + "')";
